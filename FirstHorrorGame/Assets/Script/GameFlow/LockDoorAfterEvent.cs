d7730c651e5f75d486bbc2ad66e62551103fa24d
Enzeun <khuynjun123@naver.com>
2026-08-24 18:28:41 +0900

    [Feat] 특정 이벤트 종료 후 문 닫고 잠그는 기능 확장

---
+++ FirstHorrorGame/Assets/Script/GameFlow/LockDoorAfterEvent.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+namespace UHFPS.Runtime
+{
+
+    public class LockDoorAfterEvent : MonoBehaviour
+    {
+        [BoxGroup("목표 문")]
+        [SerializeField] private DynamicObject doorToLock;
+
+        [BoxGroup("추적 디버깅")]
+        [SerializeField, ReadOnly] private bool Started = false;
+        [BoxGroup("추적 디버깅")]
+        [ShowInInspector, ReadOnly] public bool Finished { get; private set; } = false;
+
+        [BoxGroup("이벤트")]
+        public UnityEvent OnFinished;
+
+        [BoxGroup("디버깅"), Button]
+        public void LockDoor()
+        {
+            if (doorToLock == null || Started)
+                return;
+
+            Started = true;
+
+            if (doorToLock.IsOpened)
+            {
+                doorToLock.useEvent2.AddListener(WaitForAnimation);
+                doorToLock.InteractStartPlayer(gameObject);
+            }
+            else
+            {
+                FinishMethod();
+            }
+        }
+
+        private void WaitForAnimation()
+        {
+            doorToLock.useEvent2.RemoveListener(WaitForAnimation);
+
+            FinishMethod();
+        }
+        private void FinishMethod()
+        {
+            doorToLock.SetLockedStatus(true);
+
+            Finished = true;
+
+            OnFinished?.Invoke();
+        }
+    }
+}
